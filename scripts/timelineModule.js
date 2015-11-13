56099981b25c42f67a6e8542cbcdacf5a6e75ad4
Murat1202 <murat.gueloenue@gmx.net>
2015-11-13 02:29:39 +0000

    Timelinemodule

--- scripts/timelineModule.js
+++ scripts/timelineModule.js
@@ -48,5 +48,59 @@ angular.module('timelineModule', ['angularAwesomeSlider'])
     }]);
 
 function genVisYearTimeline(data) {
-	console.log(data);
+
+    svg = d3.select("overviewLinechart").append("svg").attr("width", width).attr("height", height).append("g")
+        .attr("transform", "translate(" + margins.left + "," + margins.top + ")");
+
+    var daten = [
+        {'spieltag': 1, 'tore': 3},
+        {'spieltag': 2, 'tore': 5},
+        {'spieltag': 3, 'tore': 2},
+        {'spieltag': 4, 'tore': 7},
+        {'spieltag': 5, 'tore': 12},
+    ]
+
+     var width = 300;
+     var height = 200;
+     var xFaktor=30;
+     var yFaktor=20;
+
+var svgElement= d3.select('overviewLinechart').append(svg).attr({
+   
+    'width': width,
+    'height': height,
+
+});
+
+
+var pfad=
+    d3.svg.line()
+        .x(function (d){
+            return d.spieltag*xFaktor;
+        })
+        .y(function (d){
+            return height - d.tore*yFaktor;
+        });
+        .interpolate('basis')
+
+svgElement
+    .append('path')
+    .attr ({
+        'd': pfad(daten),
+        'fill': 'none',
+        'stroke': 'black',
+        'stroke-width': 4
+         //dicke des stiftes
+
+
+    });
+
+
+
 }
+
+
+
+
+
+
