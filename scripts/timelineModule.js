be914b9c27bc153c7cfbb718cf8d9cf7b871365c
cschnoeink <christian.schnoink@tecnico.ulisboa.pt>
2015-11-16 09:14:24 +0000

    fix timelineModule.js

--- scripts/timelineModule.js
+++ scripts/timelineModule.js
@@ -152,22 +152,7 @@ function genVisYearTimeline(data) {
     .attr('fill', function (d){
         return  'blue';
     });
- 
-    svg
-        .selectAll('text')
-        .data(data)
-        .enter()
-        .append("text")
-        .text("Applicants: " + d.applicants)
-        .attr("class", "text-label")
-        .style("fill", "#1A242F")
-        .style("font-size", "20")
-        .attr("x", function(d) {
-            return x (d.year) ;
-        })
-        .attr("y", function(d) {
-            return y (d.applicants);
-        });
+
 
    
  svg
