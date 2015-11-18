e9dfee0ade073bbe7243716ee97ae91f1a487925
cschnoeink <christian.schnoink@tecnico.ulisboa.pt>
2015-11-18 15:55:41 +0000

    update countriesModule.js (get data)

--- scripts/countriesModule.js
+++ scripts/countriesModule.js
@@ -1,4 +1,28 @@
 angular.module('countriesModule', [])
     .controller('countriesCtrl', ['$scope', '$http', function($scope, $http) {
 
+        var emigrants;
+        var immigrants;
+        var i, j;
+
+        $http.get('data/applicants_gdp.json')
+            .then(function(res) {
+                immigrants = res.data.data;
+                var displayData = [];
+                for (i = 0; i < immigrants.length; i++) {
+                    if (immigrants[i].year == 2015) {
+                        displayData.push(immigrants[i]);
+                    }
+                }
+                displayData.sort(function(a, b) {
+                    return b.applicants - a.applicants;
+                });
+                /* put d3 functions here */
+            });
+
+        $http.get('data/emigrants.json')
+            .then(function(res) {
+                emigrants = res.data.data;
+                /* put d3 funtions here */
+            });
     }]);
