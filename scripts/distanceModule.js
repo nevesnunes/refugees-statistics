3a947a65b58d0598d502f910f8eb237c17351b44
Francisco Nunes <francisco.m.nunes@tecnico.ulisboa.pt>
2015-11-19 13:17:35 +0000

    Globe rotation; Selected country with color fill

--- scripts/distanceModule.js
+++ scripts/distanceModule.js
@@ -14,6 +14,9 @@ function genVis(error, world, names) {
     if (error) throw error;
 
     var width = 800, height = 800;
+    var svg = d3.select("#globe").append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
     var projection = d3.geo.azimuthalEquidistant()
         .scale(100)
@@ -21,16 +24,10 @@ function genVis(error, world, names) {
         .clipAngle(180 - 1e-3)
         .precision(.1);
 
-    var path = d3.geo.path()
-        .projection(projection);
+    var path = d3.geo.path().projection(projection);
 
-    var graticule = d3.geo.graticule();
+    //// Globe lines (graticule)
 
-    var svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-    // Globe lines
     svg.append("defs").append("path")
         .datum({type: "Sphere"})
         .attr("id", "sphere")
@@ -44,72 +41,72 @@ function genVis(error, world, names) {
         .attr("class", "fill")
         .attr("xlink:href", "#sphere");
 
+    var graticule = d3.geo.graticule();
     svg.append("path")
         .datum(graticule)
         .attr("class", "graticule")
         .attr("d", path);
 
-
-/*
-      svg.insert("path", ".graticule")
-          .datum(topojson.feature(world, world.objects.land))
-          .attr("class", "land")
-          .attr("d", path);
-
-      svg.insert("path", ".graticule")
-          .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
-          .attr("class", "boundary")
-          .attr("d", path);
-*/
     d3.select(self.frameElement).style("height", height + "px");
 
-    var countries = topojson.feature(world, world.objects.countries).features,
-      neighbors = topojson.neighbors(countries),
-      i = -1,
-      n = countries.length;
+    //// Countries
 
-  countries.forEach(function(d) { 
-    var tryit = names.filter(function(n) { return d.id == n.id; })[0];
-    if (typeof tryit === "undefined"){
-      d.name = "Undefined";
-    } else {
-      d.name = tryit.name; 
-    }
-  });
+    // Assign country names
+    var countries = topojson.feature(world, world.objects.countries).features;
+    countries.forEach(function(d) { 
+        var tryit = names.filter(function(n) { return d.id == n.id; })[0];
+        if (typeof tryit === "undefined"){
+          d.name = "Undefined";
+        } else {
+          d.name = tryit.name; 
+        }
+    });
 
-var country = svg.selectAll(".country").data(countries);
-  country
-   .enter()
-    .insert("path")
-    .attr("class", "country")    
-      .attr("title", function(d,i) { return d.name; })
-      .attr("d", path)
-      .style("fill", "#ddd");
-
-    // Show/hide tooltip
+    // Displays country name on the map
     var tooltip = d3.select("body").append("div")
         .attr("class", "tooltip");
+    
+    // Assign country data, with key = id
+    var country = svg.selectAll(".country").data(countries, function(d) {
+        return d.id;
+    });
     country
-      .on("click", function(d,i) {
-        //var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
-        //console.log(mouse[0]);
-        console.log(d3.geo.centroid(countries[i]));
-       /* 
-        country
-            .enter()
-            .attr("title", function(target_d, target_i) {return target_d.name == d.name})
-            .style("fill", "red")
-*/
-        //.style("fill", "red");
+        .enter()
+        .insert("path")
+        .attr("d", path)
+        .attr("class", "land")
+
+        // Rotate globe to selected country
+        .on("click", function(d,i) {
+            var p = d3.geo.centroid(countries[i]);
+            (function transition() {
+                d3.transition().duration(750).tween("rotate", function() {
+                    var r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
+                    return function(t) {
+                        projection.rotate(r(t));
+                        svg.selectAll("path")
+                            .attr("d", path)
+                            .classed("land-selected", function(d2, i) {
+                                return d2.id == d.id;
+                            });
+                    };
+                });
+            })();
         })
-      .on("mousemove", function(d,i) {
-        var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
-        tooltip
-          .classed("hidden", false)
-          .attr("style", "left:"+(mouse[0]+25)+"px;top:"+(mouse[1]+150)+"px")
-          .html(d.name);
-      })
-      .on("mouseout",  function(d,i) {
-        tooltip.classed("hidden", true)
-      });
+
+        // Show tooltip
+        .on("mousemove", function(d,i) {
+            var mouse = d3.mouse(svg.node()).map(function(d) {
+                return parseInt(d);
+            });
+            tooltip
+                .classed("hidden", false)
+                .attr("style", "left:"+(mouse[0]+25)+"px;top:"+(mouse[1]+150)+"px")
+                .html(d.name);
+        })
+
+        // Hide tooltip
+        .on("mouseout",  function(d,i) {
+            tooltip.classed("hidden", true)
+        });
 }
